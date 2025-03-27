581c6e6f4fac566ab7e1955f67875311f096c2ce
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-03-27 19:17:20 +0100

    Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,3 +1,33 @@
-const loadHandler = () => {};
+import { BASE_API_URL, jobDetailsContentEl, getData } from "../common.js";
+import renderSpinner from "./Spinner.js";
+import renderJobDetails from "./JobDetails.js";
+import renderError from "./Error.js";
+const loadhashChangeHandler = async () => {
+  //get id from URL
+  const id = window.location.hash.substring(1);
+  if (id) {
+    //   remove previous job details content
+    jobDetailsContentEl.innerHTML = "";
+    // add spinner
+    renderJobDetails(jobItem);
+    try {
+      // fetch job item data
+      const data = await getData(`${BASE_API_URL}/jobs/${id}`);
 
-window.addEventListener("DOMContentLoaded", loadHandler);
+      // extract job item
+      const { jobItem } = data;
+
+      // remove spinner
+      renderSpinner("job-details");
+
+      // render job details
+      renderJobDetails(jobItem);
+    } catch (error) {
+      renderSpinner("job-details");
+      renderError(error.message);
+    }
+  }
+};
+
+window.addEventListener("DOMContentLoaded", loadhashChangeHandler);
+window.addEventListener("hashchange", loadhashChangeHandler);
