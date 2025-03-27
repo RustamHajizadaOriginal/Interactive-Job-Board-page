f6967150678a5415b8a9d4ac8a804a8930fcc88d
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-03-27 08:54:18 +0100

    sorting compninet Part 1(Sorting data & State)

---
+++ src/components/Sorting.js
@@ -0,0 +1,21 @@
+import {
+  sortingEl,
+  sortingBtnRelevantEl,
+  sortingBtnRecentEl,
+} from "../common.js";
+
+const clickHandler = (event) => {
+  // get clicked button element
+  const clickButtonEl = event.target.closest(".sorting__button");
+
+  // stop function if no clicked button element
+  if (!clickButtonEl) return;
+  // check if intention is recent or relevant sorting
+  clickButtonEl.className.includes("--recent") ? true : false;
+  // sort job items
+  if (recent) {
+  } else {
+  }
+};
+
+sortingEl.addEventListener("click", clickHandler);
